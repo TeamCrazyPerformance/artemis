dd9e4d4898e1d296a008d8f6a8b599dd6afb31db
geine <bonos2@wisdo.me>
2018-03-04 01:08:31 +0900

    인덱스 컨트롤러 추가

---
+++ src/main/java/com/genie/artemis/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.genie.artemis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 인덱스 컨트롤러
+ *
+ * Created by geine on 2018. 3. 4..
+ */
+@Controller
+public class IndexController {
+
+  @GetMapping(value = "/index", produces = "text/html")
+  public String index() {
+    return "index";
+  }
+}
